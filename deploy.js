5db6fe3dd50fe976a50303a11f5b25fb6aaed3b8
EGOIST <0x142857@gmail.com>
2016-02-10 23:12:52 +0800

    publish to gh-pages

---
+++ deploy.js
@@ -0,0 +1,11 @@
+const gh = require('gh-pages')
+require('shelljs/global')
+
+exec('tooling build -e src --title 福利')
+cp('-rf', 'CNAME', './build/')
+gh.publish('./build', err => {
+	if (err) {
+		return console.log(err)
+	}
+	console.log('Done!')
+})
